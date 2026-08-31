0dae8b8109f0ae03f27781613e903aa3e719a826
Mattiel <devmattiel@gmail.com>
2026-08-31 13:35:56 -0500

    Agregue el logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import Logo from "../assets/Logo.png"
+
 function Header() {
     return (
         <header className="header">
             <div className="header-inner">
                 <div className="brand-wrap">
-                    <span className="brand-mark">C</span>
+                    <img className="brand-mark" src={Logo} alt="Logo de la Marca" />
+
                     <h1 className="title">Crediby</h1>
                 </div>
 
